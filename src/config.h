ec293958744968a4d4996e849a9f24e0c2966df5
LockBall <johnplutz@gmail.com>
2024-03-30 09:44:20 -0400

    loop generation of arrays based on config file values

--- src/config.h
+++ src/config.h
@@ -1,5 +1,21 @@
+
+const int start_dig_pin = 2 ;
+const int end_dig_pin = 13 ;
+const int num_dig_pins = end_dig_pin - start_dig_pin + 1 ;  // nano, 12 pins, 2 → 13
+//const int dig_pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13} ;
+
+const int dig_pins_out[] = {13} ;
+const int num_dig_pins_out = sizeof(dig_pins_out) / sizeof(dig_pins_out[0]) ;
+
+const int dig_pins_in[] = {} ;
+const int num_dig_pins_in = sizeof(dig_pins_in) / sizeof(dig_pins_in[0]) ;
+
+const int board_LED_pin = 13 ;
+
 const long baud = 115200 ;
-bool debug = 0;
+
+bool debug = 1;
+
 bool heartbeat = 0;
-const long interval = 500;  // interval at which to blink (milliseconds)
+const long interval = 1000;  // interval at which to blink (milliseconds)
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,52 +2,58 @@
 #include <Arduino.h>
 #include <config.h>
 
-unsigned long previous_millis = 0;  // will store last time LED was updated
-bool state_13 = 0;
-int incomingByte = 0;
+unsigned long previous_millis = 0 ;  // store time of last LED update
+int incomingByte = 0 ;
+int dig_pins_out_vals[end_dig_pin + 1] = {};  // each pin # val will be at that index # in the array
 
 void setup() {  // the setup function runs once when you press reset or power the board
-    pinMode(LED_BUILTIN, OUTPUT) ;
-    digitalWrite(LED_BUILTIN, state_13);
+    
+    for (int i = 0; i <= num_dig_pins_out; i++) {  // set pins to output mode
+        pinMode(dig_pins_out[i], OUTPUT) ;
+    
+        dig_pins_out_vals[dig_pins_out[i]] = 0 ; // set output pins vals
+        digitalWrite(dig_pins_out[i], dig_pins_out_vals[dig_pins_out[i]]); // write output pins to 0. pin_#, value
+    }
 
     Serial.begin(baud) ;
     Serial.setTimeout(100) ; // msec
 }
 
+
 void loop() {
 
-    if (Serial.available() > 0) {  // read the incoming byte:
+    if (Serial.available() > 0) {  // read incoming byte, an int that is the pin number
         incomingByte = Serial.parseInt();
 
-        if (debug == 1) {
-            Serial.print("I received: ");
-            Serial.println(incomingByte);
+
+        if (incomingByte >= start_dig_pin && incomingByte <= end_dig_pin) {
+            Serial.print(dig_pins_out_vals[incomingByte]);
+            // toggle the value of the pin
+            dig_pins_out_vals[incomingByte] = !dig_pins_out_vals[incomingByte];
+            digitalWrite(incomingByte, dig_pins_out_vals[incomingByte]); // pin, value
+            Serial.print(dig_pins_out_vals[incomingByte]);
         }
+
+        if (debug == 1) {
+            Serial.print("\n I received: ");
+            Serial.println(incomingByte);
+
+            Serial.print("\n dig_pins_out_vals: ");
+            for (int i = 0; i <= end_dig_pin; i++) {
+                Serial.print(dig_pins_out_vals[i]) ;
+            }     
+        }
+
     }
 
 
-    switch (incomingByte) {
-
-        case 13:
-            //Serial.print("case 13");
-            state_13 = !state_13;
-            digitalWrite(LED_BUILTIN, state_13);
-            incomingByte = 0;
-            break;
-        
-        default:
-            break;
-        
-    }
-
-
-    if (heartbeat == 1){  // BEGIN optional heartbeat LED
+    if (heartbeat == 1) {  // BEGIN optional heartbeat LED
         unsigned long current_millis = millis();
 
         if (current_millis - previous_millis >= interval) {
             previous_millis = current_millis;  // save the last time you blinked the LED
-            state_13 = !state_13 ;  // alternate LED_state
-            digitalWrite(LED_BUILTIN, state_13) ;
+            dig_pins_out_vals[board_LED_pin] = !dig_pins_out_vals[board_LED_pin] ;
+            digitalWrite(board_LED_pin, dig_pins_out_vals[board_LED_pin]) ;
             //Serial.print(current_millis);
         }
     }  // END hearbeat LED
